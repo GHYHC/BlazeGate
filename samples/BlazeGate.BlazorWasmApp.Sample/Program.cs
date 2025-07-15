44db739f01fbaa6ea1ee88328daa37aa69b7053a
yhc <807605906@qq.com>
2025-07-15 10:01:17 +0800

    fix

--- samples/BlazeGate.BlazorWasmApp.Sample/Program.cs
+++ samples/BlazeGate.BlazorWasmApp.Sample/Program.cs
@@ -1,7 +1,10 @@
 using AntDesign.ProLayout;
 using BlazeGate.BlazorWasmApp.Sample.Api;
+using BlazeGate.Model.Culture;
 using BlazeGate.RBAC.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 namespace BlazeGate.BlazorWasmApp.Sample
 {
@@ -19,7 +22,32 @@ namespace BlazeGate.BlazorWasmApp.Sample
             builder.Services.AddBlazeGateRBAC();
             builder.Services.AddScoped<WebApi>();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            await SetCultureAsync(host, LanguageOptions.Languages[0]);
+
+            await host.RunAsync();
+        }
+
+        /// <summary>
+        /// 设置应用程序的文化信息
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="defaultCulture"></param>
+        /// <returns></returns>
+        private static async Task SetCultureAsync(WebAssemblyHost host, string defaultCulture)
+        {
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var result = await js.InvokeAsync<string>("blazorCulture.get");
+            var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
+
+            if (result == null)
+            {
+                await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
+            }
+
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
         }
     }
 }
